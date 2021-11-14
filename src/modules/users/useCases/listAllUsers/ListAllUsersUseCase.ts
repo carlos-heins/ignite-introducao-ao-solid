fe353f318bf9c2d313134ce0dd590b2a2c9add16
carlos-heins <Carlozheins053@gmail.com>
2021-11-14 18:27:29 -0300

    UseCases and Repositorys ready

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,27 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const user = this.usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new Error("User does not exists!");
+        }
+
+        if (user.admin === false) {
+            throw new Error("Must be Admin to acess the list of users");
+        }
+
+        const users = this.usersRepository.list();
+
+        return users;
+    }
 }
 
 export { ListAllUsersUseCase };
