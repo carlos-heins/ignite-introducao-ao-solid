4153a7f6e73f7fb8d70dbc53b4595c051847fd1c
carlos-heins <Carlozheins053@gmail.com>
2021-11-28 05:19:04 -0300

    Documentation with swagger.io

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () => {
+    console.log("Roodaaando");
+    console.log("Documentation: http://localhost:3333/api-docs");
+});
